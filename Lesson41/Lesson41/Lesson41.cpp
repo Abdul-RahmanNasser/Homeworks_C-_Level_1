a30d0ea7bee35e36af66ee64d8c01b98838ff022
Abdul-Rahman <bodysttar@gmail.com>
2024-12-27 17:03:09 +0200

    the problem solved on lesson 41 is modified and developed to be the problem solved on lesson 48.

--- Lesson41/Lesson41/Lesson41.cpp
+++ Lesson41/Lesson41/Lesson41.cpp
@@ -6,13 +6,13 @@ struct personalinfo {
 	int age, phone;
 };
 void read_info(personalinfo &info) {
-	cout << "Please, enter first name: ";
+	cout << "Please, enter the first name: ";
 	cin >> info.first_name;
-	cout << "Please, enter last name: ";
+	cout << "Please, enter the last name: ";
 	cin >> info.last_name;
-	cout << "Please, enter age: ";
+	cout << "Please, enter the age: ";
 	cin >> info.age;
-	cout << "Please, enter phone number: ";
+	cout << "Please, enter the phone number: ";
 	cin >> info.phone;
 }
 void print_info(personalinfo &info) {
@@ -23,18 +23,36 @@ void print_info(personalinfo &info) {
 	cout << "Phone number: " << info.phone << "." << endl;
 	cout << "***********************************************" << endl;
 }
-void read_personal_info(personalinfo info[2]) {
-	read_info(info[0]);
-	read_info(info[1]);
+void read_personal_info(personalinfo info[100], int& length) {
+	cout << "Please, enter the number of the persons you want: ";
+	cin >> length;
+	if (length <= 100 && length >= 1) {
+		for (int i = 1; i <= length; i++) {
+			cout << "\nPlease, enter the information for the person number " << i << ":\n";
+			cout << "***************************************************" << endl;
+			read_info(info[i]);
+			cout << "***************************************************" << endl << endl;
+		}
+	}
+	else {
+		cout << "Invalid input! the allowed range is the integers [1,100].";
+	}
 }
-void print_personal_info(personalinfo info[2]) {
-	print_info(info[0]);
-	print_info(info[1]);
+void print_personal_info(personalinfo info[100], int length) {
+	if (length <= 100 && length >= 1) {
+		cout << "The information for the persons is:" << endl << endl;
+		for (int i = 1; i <= length; i++) {
+			cout << "The information for the person number " << i << " is:" << endl;
+			print_info(info[i]);
+			cout << endl;
+		}
+	}
 }
 int main()
 {
-	personalinfo persons[2];
-	read_personal_info(persons);
-	print_personal_info(persons);
+	int length;
+	personalinfo persons[100];
+	read_personal_info(persons, length);
+	print_personal_info(persons, length);
 	return 0;
 }
